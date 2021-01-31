e3f42957dee5abf61652ea410890c2ad9c5035be
Ranjeet Singh <rsdhiman1998@gmail.com>
2021-01-31 01:40:10 -0500

    Initial Commit

---
+++ A2_FA_Ranjeet_C0785585/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  A2_FA_Ranjeet_C0785585
+//
+//  Created by Ranjeet Singh on 31/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
